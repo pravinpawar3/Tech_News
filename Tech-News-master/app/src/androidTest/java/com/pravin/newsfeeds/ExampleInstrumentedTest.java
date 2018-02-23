3807d9fed7e63d059dce1ddefa3440bd10ba180e
pravinpawar3 <pravinpawar381997@gmail.com>
2018-02-23 16:00:50 +0530

    Update ExampleInstrumentedTest.java

--- Tech-News-master/app/src/androidTest/java/com/pravin/newsfeeds/ExampleInstrumentedTest.java
+++ Tech-News-master/app/src/androidTest/java/com/pravin/newsfeeds/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.gadhvi.newsfeeds", appContext.getPackageName());
+        assertEquals("com.pravin.newsfeeds", appContext.getPackageName());
     }
 }
